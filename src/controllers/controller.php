198a27f26fe584f8eb69c4f8e95dbf623267f3ef
Matheus Brizolla de Souza <matheus.souza@duque.g12.br>
2025-04-10 19:42:28 -0300

    Criação de controller

--- src/controllers/controller.php
+++ src/controllers/controller.php
@@ -3,11 +3,29 @@
 
 // Função que manipula as requsições do servidor
 function handleRequest(){
-    //Seu código aqui
+    // Seu código aqui
+    if(isset($_POST['action'])){
+        switch($_POST['action']){
+            case 'add':
+                if(!empty($_POST['description'])){
+                    addNewTask($_POST['description']);
+                }
+                break;
+
+            case 'toggle':
+                if(!empty($_POST['id'])){
+                    toggleTask($_POST['id']);
+                }
+                break;
+        }
+    }
 }
+
 
 // Função que exibe as tasks na view
 function showTasks(){
     // Seu código aqui
+    $tasks = getTasks();
+    require_once __DIR__.'/../views/tasks.php'
 }
 ?>
